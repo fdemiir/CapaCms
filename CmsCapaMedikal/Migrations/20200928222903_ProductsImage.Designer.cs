a86fcba64605ec2d99d31a9fcce50b90cba02e59
fdemiir <fdemir3460@gmail.com>
2020-09-29 02:20:29 +0300

    NO-ISSUE / DB connectionstring düzenlemesi

--- CmsCapaMedikal/Migrations/20200928222903_ProductsImage.Designer.cs
+++ CmsCapaMedikal/Migrations/20200928222903_ProductsImage.Designer.cs
@@ -3,14 +3,16 @@ using CmsCapaMedikal.Helper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CmsCapaMedikal.Migrations
 {
     [DbContext(typeof(ProductsContext))]
-    partial class ProductsContextModelSnapshot : ModelSnapshot
+    [Migration("20200928222903_ProductsImage")]
+    partial class ProductsImage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
