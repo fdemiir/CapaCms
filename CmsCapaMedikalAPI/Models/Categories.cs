22bd09db0030511e868a8d0969363d5cae97b02c
fdemiir <fdemir3460@gmail.com>
2020-09-30 20:40:22 +0300

    NO-ISSUE / model guncelleme

---
+++ CmsCapaMedikalAPI/Models/Categories.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsCapaMedikalAPI.Models
+{
+    public class Categories
+   {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryPath { get; set; }
+        public string CategoryUrl { get; set; }
+        public string CategoryInfo { get; set; }
+        [NotMapped]
+        public List<Products> Items { get; set; }
+    }
+}
---
+++ CmsCapaMedikalAPI/Models/Products.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsCapaMedikalAPI.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string ProductCode { get; set; }
+        public string ProductName { get; set; }
+        public string ProductArea { get; set; }
+        public string ProductClass { get; set; }
+        public string ProductType { get; set; }
+        public string ProductBottomBrand { get; set; }
+        public string ProductImage { get; set; }
+        public string ProductCategoryName { get; set; }
+
+    }
+
+}
